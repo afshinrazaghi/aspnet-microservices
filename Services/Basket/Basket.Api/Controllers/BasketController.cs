245dafab3f64fd1ab2d20af5e9a61b51958ac7eb
razaghi <neo1.afshin@gmail.com>
2023-07-12 17:16:06 +0330

    Add Basket Repository Create Basket Controller Partially

---
+++ Services/Basket/Basket.Api/Controllers/BasketController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+
+    }
+}
---
+++ Services/Basket/Basket.Api/Repositories/BasketRepository.cs
@@ -0,0 +1,37 @@
+﻿using Basket.Api.Entities;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.Json.Serialization;
+
+namespace Basket.Api.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDistributedCache _redisCache;
+
+        public BasketRepository(IDistributedCache cache)
+        {
+            _redisCache = cache ?? throw new ArgumentNullException(nameof(cache));
+        }
+
+        public async Task DeleteShoppingCart(string userName)
+        {
+            await _redisCache.RemoveAsync(userName);
+        }
+
+        public async Task<ShoppingCart?> GetBasket(string userName)
+        {
+            var basket = await _redisCache.GetStringAsync(userName);
+            if (string.IsNullOrEmpty(basket))
+                return null;
+            return JsonConvert.DeserializeObject<ShoppingCart>(basket);
+        }
+
+        public async Task<ShoppingCart?> UpdateBasket(ShoppingCart basket)
+        {
+            await _redisCache.SetStringAsync(basket.UserName, JsonConvert.SerializeObject(basket));
+            return await GetBasket(basket.UserName);
+        }
+    }
+}
