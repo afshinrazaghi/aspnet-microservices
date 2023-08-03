558b22601a6c1fb1441071c7cac9d63adb1879d8
Afshin Razaghi <neo1.afshin@gmail.com>
2023-08-03 19:12:36 +0330

    Add Repository to Dscount.Api Complete Configuration for docker

---
+++ Services/Discount/Discount.Api/Entities/Coupon.cs
@@ -0,0 +1,12 @@
+﻿#pragma warning disable
+
+namespace Discount.Api.Entities
+{
+    public class Coupon
+    {
+        public long Id { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
+    }
+}
